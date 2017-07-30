5309fe21f7ea3052bc9f3011e54f8507a514cf38
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2017-07-30 16:40:35 +0900

    response

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
-# A sample Gemfile
 source "https://rubygems.org"
 
 gem "rack"
+
+gem "pry"
+gem "pry-byebug"
--- app.rb
+++ app.rb
@@ -1,24 +1,24 @@
 # coding: utf-8
 
 require 'rack'
+require 'rack/request'
+require 'rack/response'
+require 'rack/query_parser'
+
+require 'pry'
 
 class App
   def call(env)
-    p env
 
-    case env['REQUEST_METHOD']
-    when 'GET'
-      [
-        200,
-        { 'Content-Type' => 'text/html' },
-        ['<html><body><form method="POST"><input type="submit" value="見たい?" /></form></body></html>']
-      ]
-    when 'POST'
-      [
-        200,
-        { 'Content-Type' => 'text/html' },
-        ['<html><body>何見てんだよ</body></html>']
-      ]
-    end
+    req = Rack::Request.new env
+    query_parser = Rack::QueryParser.make_default 10, 10
+
+    p query_parser.parse_nested_query req.query_string
+
+    res = Rack::Response.new { |r|
+      r.status = 200
+    }
+
+    res.finish
   end
 end
