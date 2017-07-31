d27bf08db1af413e8c692088ab96ed60730a7aa1
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2017-07-31 22:27:09 +0900

    slackに対応

--- app.rb
+++ app.rb
@@ -14,6 +14,7 @@ Dotenv.load ".env"
 
 class App
   @@channel_access_token = ENV["CHANNEL_ACCESS_TOKEN"]
+  @@slack_incoming_webhook = ENV["SLACK_INCOMING_WEBHOOK"]
 
   def call(env)
     p env["HTTP_X_LINE_SIGNATURE"]
@@ -21,8 +22,24 @@ class App
 
     req = Rack::Request.new env
 
+    p req
+
     if req.post?
+      conn = Faraday.new url: @@slack_incoming_webhook do |builder|
+        builder.use Faraday::Request::UrlEncoded  # リクエストパラメータを URL エンコードする
+        builder.use Faraday::Response::Logger     # リクエストを標準出力に出力する
+        builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
+
       end
+
+      slack_res = conn.post do |slack_res|
+        slack_res.body = "payload=" + JSON.dump({
+          text: 'foo'
+        })
+      end
+
+      p slack_res
+
     elsif req.get?
       query_parser = Rack::QueryParser.make_default 10, 10
       p query_parser.parse_nested_query req.query_string
