b1e8fcdba2221926f6331d8c68b9eabfd89df8bf
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2017-07-31 23:35:16 +0900

    Slackを抽出

--- app.rb
+++ app.rb
@@ -24,21 +24,7 @@ class App
 
     p req
 
-    if req.post?
-      conn = Faraday.new url: @@slack_incoming_webhook do |builder|
-        builder.use Faraday::Request::UrlEncoded  # リクエストパラメータを URL エンコードする
-        builder.use Faraday::Response::Logger     # リクエストを標準出力に出力する
-        builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
 
-      end
-
-      slack_res = conn.post do |slack_res|
-        slack_res.body = "payload=" + JSON.dump({
-          text: 'foo'
-        })
-      end
-
-      p slack_res
 
     elsif req.get?
       query_parser = Rack::QueryParser.make_default 10, 10
---
+++ slack.rb
@@ -0,0 +1,23 @@
+require 'dotenv'
+require 'faraday'
+
+class Slack
+  @@slack_incoming_webhook = ENV["SLACK_INCOMING_WEBHOOK"]
+
+  def response req
+    conn = Faraday.new url: @@slack_incoming_webhook do |builder|
+      builder.use Faraday::Request::UrlEncoded  # リクエストパラメータを URL エンコードする
+      builder.use Faraday::Response::Logger     # リクエストを標準出力に出力する
+      builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
+
+    end
+
+    slack_res = conn.post do |slack_res|
+      slack_res.body = "payload=" + JSON.dump({
+        text: 'foo'
+      })
+    end
+
+    p slack_res
+  end
+end
