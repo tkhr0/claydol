b5e66d7f91c6ff116a5631247a4f3625a944685d
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2017-07-31 22:24:54 +0900

    lineを抽出

--- app.rb
+++ app.rb
@@ -22,34 +22,6 @@ class App
     req = Rack::Request.new env
 
     if req.post?
-      line_request = JSON.parse(req.body.read)
-      p line_request
-      line_request["events"].each do |event|
-
-        message = event["message"]
-
-        p message
-
-        conn = Faraday.new url: 'https://api.line.me/v2/bot/message/reply' do |builder|
-          builder.use Faraday::Request::UrlEncoded  # リクエストパラメータを URL エンコードする
-          builder.use Faraday::Response::Logger     # リクエストを標準出力に出力する
-          builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
-        end
-
-        line_res = conn.post do |line_req|
-          line_req.headers["Content-Type"] = 'application/json'
-          line_req.headers["Authorization"] = "Bearer #{@@channel_access_token}"
-
-          line_req.body = JSON.dump({
-            replyToken: event["replyToken"],
-            messages: [
-              {type: "text", text: message['text']}
-            ]
-          })
-          p line_req
-        end
-
-        p line_res
       end
     elsif req.get?
       query_parser = Rack::QueryParser.make_default 10, 10
---
+++ line.rb
@@ -0,0 +1,40 @@
+require 'dotenv'
+require 'faraday'
+
+Dotenv.load ".env"
+
+class Line
+  @@channel_access_token = ENV["CHANNEL_ACCESS_TOKEN"]
+
+  def response req
+    line_request = JSON.parse(req.body.read)
+    p line_request
+    line_request["events"].each do |event|
+
+      message = event["message"]
+
+      p message
+      conn = Faraday.new url: 'https://api.line.me/v2/bot/message/reply' do |builder|
+        builder.use Faraday::Request::UrlEncoded  # リクエストパラメータを URL エンコードする
+        builder.use Faraday::Response::Logger     # リクエストを標準出力に出力する
+        builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
+      end
+
+      line_res = conn.post do |line_req|
+        line_req.headers["Content-Type"] = 'application/json'
+        line_req.headers["Authorization"] = "Bearer #{@@channel_access_token}"
+
+        line_req.body = JSON.dump({
+          replyToken: event["replyToken"],
+          messages: [
+            {type: "text", text: message['text']}
+          ]
+        })
+        p line_req
+      end
+
+      p line_res
+    end
+  end
+
+end
