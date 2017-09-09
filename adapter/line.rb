0bdb0f8020c3b18c2cc13c9570cdd83cf46398a3
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2017-09-09 16:32:38 +0900

    line adapter inherited adapter

--- adapter/line.rb
+++ adapter/line.rb
@@ -1,40 +1,40 @@
 require 'dotenv'
+require_relative './adapter'
 require 'faraday'
 
 Dotenv.load ".env"
 
-class Line
+class Line < Adapter
   @@channel_access_token = ENV["CHANNEL_ACCESS_TOKEN"]
 
-  def response req
-    line_request = req
-    p line_request
-    line_request["events"].each do |event|
+  def initialize
+    super
+    @endpoint = 'https://api.line.me/v2/bot/message/reply'
+  end
 
-      message = event["message"]
+  def decode params
+    synapse.message = params['events'][0]['message']['text']
+    synapse.token = params['events'][0]['replyToken']
+    synapse
+  end
 
-      p message
-      conn = Faraday.new url: 'https://api.line.me/v2/bot/message/reply' do |builder|
-        builder.use Faraday::Request::UrlEncoded  # リクエストパラメータを URL エンコードする
-        builder.use Faraday::Response::Logger     # リクエストを標準出力に出力する
-        builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
-      end
+  def encode synapse
+    # FIXME: ヘッダの追加は別の場所でやりたい
+    header = {
+      "Content-Type": 'application/json',
+      "Authorization": "Bearer #{@@channel_access_token}"
+    }
+    add_header header
 
-      line_res = conn.post do |line_req|
-        line_req.headers["Content-Type"] = 'application/json'
-        line_req.headers["Authorization"] = "Bearer #{@@channel_access_token}"
-
-        line_req.body = JSON.dump({
-          replyToken: event["replyToken"],
-          messages: [
-            {type: "text", text: message['text']}
-          ]
-        })
-        p line_req
-      end
-
-      p line_res
-    end
+    JSON.dump({
+      replyToken: synapse.token,
+      messages: [
+        {
+          type: "text",
+          text: synapse.response
+        }
+      ]
+    })
   end
 
 end
